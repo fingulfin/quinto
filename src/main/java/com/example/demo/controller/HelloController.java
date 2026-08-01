1d75177748936045b78ed64c20e0972f3c181f5f
Juan Carlos <rapidclimate@yahoo.com>
2026-08-01 08:15:48 -0600

    Nuevo

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
     }
     @GetMapping("/")
     public String bienvenida() {
-        return "Proyecto hospedado en google cloud run";
+        return "Proyecto hospedado en google cloud run esto es muy niceeeee";
     }
 }
