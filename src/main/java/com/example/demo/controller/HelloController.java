a1c8c01c296354b17743e154d08656c507beb94d
Juan Carlos <rapidclimate@yahoo.com>
2026-08-01 09:52:17 -0600

    Con faje

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,9 @@ public class HelloController {
     @GetMapping("/")
     public String bienvenida() {
         return "Proyecto hospedado en google cloud run esto es muy niceeeee ddd";
+    }
+    @GetMapping("/fake")
+    public User fakeUser() {
+        return new User("Fake User", "fake@example.com");
     }
 }
